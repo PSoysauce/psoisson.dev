7ee34067e2e19ccb7ae275ca144271ae5fad2a69
Patrick Soisson <psoysauce@gmail.com>
2023-05-11 13:10:41 -0500

    Replaced photography page with about me

---
+++ psoisson.dev/src/components/aboutme/aboutme.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./aboutme.sass"
+
+export default function AboutMe() {
+    return (
+        <div className="AboutMe" id="aboutme">
+            <h1> More about me </h1>
+            <h4>Photo Gallery</h4>
+            <h4>Chess Profile</h4>
+        </div>
+    )
+}
