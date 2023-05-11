7b94fd091612c1fb6de6455d35855617ebc24cdb
Patrick Soisson <psoysauce@gmail.com>
2023-05-11 11:38:30 -0500

    Added to portfolio page

--- psoisson.dev/src/components/portfolio/portfolio.jsx
+++ psoisson.dev/src/components/portfolio/portfolio.jsx
@@ -1,10 +1,112 @@
-import React from 'react'
-import "./portfolio.sass"
+import React from "react";
+import "./portfolio.sass";
 
 export default function Portfolio() {
-    return (
-        <div className="Portfolio" id="portfolio">
+  return (
+    <div className="Portfolio" id="portfolio">
+      <div class="project">
+        <h1> Projects </h1>
+      </div>
+      <div class="gallery">
+        <a href="https://devpost.com/software/hackutd-kejp">
+          <img
+            src="img/receipt.png"
+            alt="Spectral Receipt"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">USAA Centennial Hackathon People’s Choice Award (2022) – MemeCoin Rewards</div>
+      </div>
+      <div class="gallery">
+        <a href="https://devpost.com/software/phantom-z648hj">
+          <img
+            src="img/phantom.jpg"
+            alt="Team Phantom"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">Hacktober (2018) - Phantom</div>
+      </div>
+      <div class="gallery">
+        <a href="https://devpost.com/software/plane-buddy">
+          <img
+            src="img/planeBuddy.jpg"
+            alt="PlaneBuddy"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">Tamuhack (2019) - Plane Buddy</div>
+      </div>
+      <div class="gallery">
+        <a href="https://devpost.com/software/hackutd-kejp">
+          <img
+            src="img/receipt.png"
+            alt="Spectral Receipt"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">HackUTD (2019) - Spectral Receipts</div>
+      </div>
+      <div class="gallery">
+        <a href="https://devpost.com/software/hackutd-kejp">
+          <img
+            src="img/receipt.png"
+            alt="Spectral Receipt"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">Tamu Datathon 2021 – Stock Prediction</div>
+      </div>
+      <div class="gallery">
+        <a href="https://devpost.com/software/hackutd-kejp">
+          <img
+            src="img/receipt.png"
+            alt="Spectral Receipt"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">UNIX Folder Watcher</div>
+      </div>
+      <div class="gallery">
+        <a href="https://devpost.com/software/hackutd-kejp">
+          <img
+            src="img/receipt.png"
+            alt="Spectral Receipt"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">HackUTD - Veri-News</div>
+      </div>
+      <div class="gallery">
+        <a href="https://devpost.com/software/hackutd-kejp">
+          <img
+            src="img/receipt.png"
+            alt="Spectral Receipt"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">Wordle Bot </div>
+      </div>
+      <div class="gallery">
+        <a href="https://github.com/PSoysauce/GroceryAssistant">
+          <img
+            src="img/receipt.png"
+            alt="Grocery Assistant"
+            width="600"
+            height="400"
+          ></img>
+        </a>
+        <div class="desc">Grocery Assistant 2020</div>
+      </div>
 
-        </div>
-    )
+    </div>
+  );
 }
