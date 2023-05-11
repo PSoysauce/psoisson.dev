e215d2e7e57f57e68e491e06dc94f60274509661
Patrick Soisson <psoysauce@gmail.com>
2023-05-11 11:38:18 -0500

    Added basic information to intro page

--- psoisson.dev/src/components/intro/intro.jsx
+++ psoisson.dev/src/components/intro/intro.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./intro.sass"
+import profilepic from '../../img/profile.JPG'
 
 export default function Intro() {
     return (
         <div className="Intro" id="intro">
-            <div></div>
+            <div class="left">
+                <img src={profilepic} class="Profile" alt="Profile"></img>
+            </div>
+            <div class="right">
+                <div>
+                    <h1>Patrick Soisson</h1>
+                    <h4>Patrick graduated from The University of Texas at Dallas in May 2021 with a Bachelor's degree in Computer Science. He is passionate about automation, documentation, learning new technologies, modernization, and being purposefully inclusive.</h4>
+                </div>
+            </div>
+            
         </div>
     )
 }
